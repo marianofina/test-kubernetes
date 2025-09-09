c3ea07c7c693892d90a2f3d871680720d9617cf5
Mariano <mariano_ef@live.com>
2025-09-09 20:10:18 -0300

    health check

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ async def read_root():
     for i in range(10_000_000):
         total += i*i
     return {"message": "Hello World", "total": total}
+
+@app.get("/health")
+async def health():
+    return "OK"
