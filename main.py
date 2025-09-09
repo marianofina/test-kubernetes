a78f93eab70b848454a08e87d5ae43d2b6f8a907
Mariano <mariano_ef@live.com>
2025-09-09 19:23:30 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    total = 0
+    for i in range(10_000_000):
+        total += i*i
+    return {"message": "Hello World", "total": total}
